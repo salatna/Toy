270acc1f65d5878c765211ff348be2308fa8da19
Antalas <anton.shkrunin@gmail.com>
2018-06-07 23:45:48 +0800

    smarter view

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,27 +27,30 @@ public class Main {
                 .addHttpListener(8080, "0.0.0.0")
                 .setHandler(
                         Handlers.exceptionHandler(exchange -> {
-                            pathTemplate()
-                                    .add("/account/{id}", in -> {
-                                        Map<String, Deque<String>> params = in.getQueryParameters();
-                                        String item = params.get("id").getFirst();
-                                        PreparedStatement query = conn.prepareStatement("SELECT AMOUNT FROM ACCOUNTS WHERE ID = ?");
-                                        query.setInt(1, parseInt(item));
-                                        query.execute();
-                                        ResultSet rs = query.getResultSet();
-                                        if (rs.next()){
-                                            in.getResponseSender().send(rs.getBigDecimal("AMOUNT").toPlainString());
-                                        }else{
-                                            exchange.setStatusCode(404);
+                                    pathTemplate()
+                                            .add("/account/{id}", in -> {
+                                                Map<String, Deque<String>> params = in.getQueryParameters();
+                                                String item = params.get("id").getFirst();
+                                                PreparedStatement query = conn.prepareStatement("SELECT AMOUNT FROM ACCOUNTS WHERE ID = ?");
+                                                query.setInt(1, parseInt(item));
+                                                query.execute();
+                                                ResultSet rs = query.getResultSet();
+                                                if (rs.next()) {
+                                                    in.getResponseSender().send(rs.getBigDecimal("AMOUNT").toPlainString());
+                                                } else {
+                                                    exchange.setStatusCode(404);
+                                                    in.getResponseSender().send("Account #" + item + " not found.");
+                                                }
+                                            })
+                                            .add("/transfer/{src}/{dst}/{amt}", in -> {
+                                                throw new RuntimeException("Implement me");
+                                            }).handleRequest(exchange);
+                                    if (exchange.isResponseChannelAvailable()) {
+                                        if (exchange.getStatusCode() == 404) {
+                                            exchange.getResponseSender().send("Not found");
                                         }
-                                    })
-                                    .add("/transfer/{src}/{dst}/{amt}", in -> {
-                                        throw new RuntimeException("Implement me");
-                                    }).handleRequest(exchange);
-                            if (exchange.getStatusCode() == 404) {
-                                exchange.getResponseSender().send("Not found");
-                            }
-                        }
+                                    }
+                                }
                         ).addExceptionHandler(Exception.class,
                                 exchange -> {
                                     if (exchange.isResponseChannelAvailable()) {
