57d3790eb8fb4a07bfd951986421c8fee036b20b
Antalas <anton.shkrunin@gmail.com>
2018-06-10 12:43:13 +0800

    persistence layer flesh out

--- src/main/java/ru/antalas/back/persistence/Persistence.java
+++ src/main/java/ru/antalas/back/persistence/Persistence.java
@@ -4,18 +4,53 @@ import java.math.BigDecimal;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
 public class Persistence {
+    //equivalent isolation level: SERIALIZED
+    private final ReadWriteLock accountsLock = new ReentrantReadWriteLock(true);
     private final Map<Integer, Account> accounts = new ConcurrentHashMap<>();
 
     public void create(ru.antalas.model.Account account) {
-        throw new RuntimeException("implement me");
+        Lock lock = accountsLock.writeLock();
+        try {
+            lock.lock();
+
+            if (accounts.containsKey(account.getId())) {
+                throw new IllegalStateException();
+            }
+
+            accounts.put(account.getId(), new Account(account));
+        } finally {
+            lock.unlock();
+        }
     }
 
     public Optional<BigDecimal> amountAt(Integer id) {
-        throw new RuntimeException("implement me");
+        Lock collectionLock = accountsLock.readLock();
+        try {
+            collectionLock.lock();
+
+            if (accounts.containsKey(id)) {
+                Account data = accounts.get(id);
+                Lock itemLock = data.lock.readLock();
+                try {
+                    itemLock.lock();
+                    return of(data.data.getBalance());
+                } finally {
+                    itemLock.unlock();
+                }
+            } else {
+                return empty();
+            }
+        } finally {
+            collectionLock.unlock();
+        }
     }
 
     public void transfer(Integer srcId, Integer dstId, BigDecimal amount) {
--- src/test/java/ru/antalas/back/persistence/PersistenceTest.java
+++ src/test/java/ru/antalas/back/persistence/PersistenceTest.java
@@ -6,6 +6,8 @@ import ru.antalas.model.Account;
 
 import java.math.BigDecimal;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
@@ -21,9 +23,19 @@ public class PersistenceTest {
         persistence.create(new Account(2, new BigDecimal("0.00")));
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void shouldErrWhenAccountExistsAtCreation() throws Exception {
+        persistence.create(new Account(1, new BigDecimal("42.00")));
+    }
+
     @Test
     public void shouldGetAmountForAccount() throws Exception {
-        assertThat(persistence.amountAt(1), is(new BigDecimal("100.00")));
-        assertThat(persistence.amountAt(2), is(new BigDecimal("0.00")));
+        assertThat(persistence.amountAt(1), is(of(new BigDecimal("100.00"))));
+        assertThat(persistence.amountAt(2), is(of(new BigDecimal("0.00"))));
+    }
+
+    @Test
+    public void shouldReportMissingAccount() throws Exception {
+        assertThat(persistence.amountAt(3), is(empty()));
     }
 }
